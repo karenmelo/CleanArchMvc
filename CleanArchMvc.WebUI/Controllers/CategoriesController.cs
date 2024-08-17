88b46e1373bf7ceb29165f117c2f5b05d8aa5ab9
Karen da Silva Melo <karen_melo@live.com>
2024-08-17 19:51:41 -0300

    Inclusao das funcionalidades de delete, details e alteracao no edit.

--- CleanArchMvc.WebUI/Controllers/CategoriesController.cs
+++ CleanArchMvc.WebUI/Controllers/CategoriesController.cs
@@ -44,7 +44,7 @@ namespace CleanArchMvc.WebUI.Controllers
         [HttpGet()]
         public async Task<IActionResult> Edit(int? id)
         {
-            if(id == null) return NotFound();
+            if (id == null) return NotFound();
 
             var category = await _categoryService.GetCategoryByIdAsync(id);
 
@@ -59,14 +59,53 @@ namespace CleanArchMvc.WebUI.Controllers
         {
             if (ModelState.IsValid)
             {
-                await _categoryService.Update(category);
+                try
+                {
+                    await _categoryService.Update(category);
+                }
+                catch (Exception ex)
+                {
+
+                    throw new ApplicationException(ex.Message);
+                }
+
                 return RedirectToAction(nameof(Index));
             }
 
             return View(category);
         }
 
+        [HttpGet()]
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null) return NotFound();
 
+            var category = await _categoryService.GetCategoryByIdAsync(id.Value);
+
+            if (category == null) return NotFound();
+
+            return View(category);
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null) return NotFound();
+
+            var category = await _categoryService.GetCategoryByIdAsync(id);
+
+            if (category == null) return NotFound();
+
+            return View(category);
+        }
+
+        [HttpPost(), ActionName("Delete")]
+        public async Task<IActionResult> DeleteConfirmed(int? id)
+        {
+            await _categoryService.Delete(id.Value);
+            return RedirectToAction(nameof(Index));
+        }
 
     }
 }
