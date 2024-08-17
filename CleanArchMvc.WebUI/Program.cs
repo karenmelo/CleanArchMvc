b694043be7ba43dc2552b78800180f54fa4b2c74
Karen da Silva Melo <karen_melo@live.com>
2024-08-17 10:13:00 -0300

    Criacao das migrations, alteracao de config do efCore no projeto Infra.Data para que a migration funcione no WebUi sem precisar instala-lo.

--- CleanArchMvc.WebUI/Program.cs
+++ CleanArchMvc.WebUI/Program.cs
@@ -1,7 +1,14 @@
+using CleanArchMvc.Infra.Ioc; 
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+
+builder.Services.AddInfrastructure(builder.Configuration);
+
+
 
 var app = builder.Build();
 
